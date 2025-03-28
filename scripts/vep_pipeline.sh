117d2d54c9317e16e03a933226a696499e634bf7
bschilder <brian_schilder@alumni.brown.edu>
2025-03-28 19:58:05 -0400

    update script

--- scripts/vep_pipeline.sh
+++ scripts/vep_pipeline.sh
@@ -15,13 +15,14 @@ conda activate esm2
 
 # Set variables
 MODELS="esm1v_t33_650M_UR90S_1"
+
 SCORING_STRATEGIES="esm1v_t33_650M_UR90S_1:wt-marginals,masked-marginals"
 HAP_DIR="$HOME/projects/VEP_protein/data/1KG/haplotypes"
 SAVE_DIR="$HOME/projects/VEP_protein/data/1KG/vep"
 VERBOSE=True
 
 # Run the VEP pipeline
-python -m $HOME/projects/VEP_protein/src/vep_pipeline \
+python -m $HOME/projects/VEP_protein/src/vep_pipeline.py \
   --models "${MODELS}" \
   --scoring_strategies "${SCORING_STRATEGIES}" \
   --hap_dir "${HAP_DIR}" \
